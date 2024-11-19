e870f752f7cd35826a4c9c9d5b0a0efedca07b43
Joe Monkila <jnkiwa25@gmail.com>
2024-11-19 09:24:42 +0100

    ui auth register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './presentation/auth-ui/login/login.component';
+import { RegisterComponent } from './presentation/auth-ui/register/register.component';
 
 export const routes: Routes = [
     {path:"", pathMatch:"full", redirectTo: "auth/login"},
-    {path: "auth/login", component: LoginComponent}
+    {path: "auth/login", component: LoginComponent},
+    {path: "auth/register", component: RegisterComponent}
 ];

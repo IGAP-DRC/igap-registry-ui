1f68d436d56b677bd028777df2b8725ff3ababeb
Joe Monkila <jnkiwa25@gmail.com>
2024-11-19 14:36:27 +0100

    (data) data module

---
+++ src/app/data/data.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from "@angular/core";
+import { UserRepository } from "../domain/repositories/user.repository";
+import { GetUserProfileUseCase } from "../domain/usecases/user/get-user-profile.usecase";
+import { UserLoginUseCase } from "../domain/usecases/user/user-login.usecase";
+import { UserRegisterUseCase } from "../domain/usecases/user/user-register.usecase";
+import { UserImplementationRepository } from "./repositories/user/user-implementation.repository";
+import { CommonModule } from "@angular/common";
+
+
+const userLoginUseCaseFactory =
+    (userRepo: UserRepository) => new UserLoginUseCase(userRepo);
+export const userLoginUseCaseProvider = {
+    provide: UserLoginUseCase,
+    useFactory: userLoginUseCaseFactory,
+    deps: [UserRepository],
+};
+
+const userRegisterUseCaseFactory =
+    (userRepo: UserRepository) => new UserRegisterUseCase(userRepo);
+export const userRegisterUseCaseProvider = {
+    provide: UserRegisterUseCase,
+    useFactory: userRegisterUseCaseFactory,
+    deps: [UserRepository],
+};
+
+const getUserProfileUseCaseFactory =
+    (userRepo: UserRepository) => new GetUserProfileUseCase(userRepo);
+export const getUserProfileUseCaseProvider = {
+    provide: GetUserProfileUseCase,
+    useFactory: getUserProfileUseCaseFactory,
+    deps: [UserRepository],
+};
+
+@NgModule({
+    providers: [
+        userLoginUseCaseProvider,
+        userRegisterUseCaseProvider,
+        getUserProfileUseCaseProvider,
+        
+        { provide: UserRepository, useClass: UserImplementationRepository },
+    ],
+    imports: [
+        CommonModule,
+    ],
+})
+export class DataModule { }
--- src/app/data/repositories/user/mappers/user-repository.mapper.ts
+++ src/app/data/repositories/user/mappers/user-repository.mapper.ts
@@ -3,7 +3,7 @@ import { UserModel } from '../../../../domain/models/user.model';
 import { UserEntity } from '../entities/user-entity';
 
 
-export class UserImplementationRepositoryMapper extends Mapper<UserEntity, UserModel> {
+export class UserImplementationRepositoryMapper extends Mapper<UserModel, UserModel> {
     mapFrom(param: UserEntity): UserModel {
         return {
             id: param.id,

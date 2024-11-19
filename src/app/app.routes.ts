5d17844e55eecb975a299c5e759bac1593ebc6e7
Joe Monkila <jnkiwa25@gmail.com>
2024-11-19 09:17:46 +0100

    ui auth login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './presentation/auth-ui/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", pathMatch:"full", redirectTo: "auth/login"},
+    {path: "auth/login", component: LoginComponent}
+];

dfa3baf3d2fb18cad5eac9c1c388ce5878a06242
Joe Monkila <jnkiwa25@gmail.com>
2024-12-08 18:19:24 +0100

    gestion agents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './presentation/auth-ui/login/login.component';
 import { RegisterComponent } from './presentation/auth-ui/register/register.component';
 import { DashboardComponent } from './presentation/dashboard/dashboard.component';
 import { AgentsComponent } from './presentation/pages/agents/agents.component';
+import { GestionAgentsComponent } from './presentation/dashboard/pages/gestion-agents/gestion-agents.component';
 
 export const routes: Routes = [
     {path:"", pathMatch:"full", redirectTo: "auth/login"},
     {path: "auth/login", component: LoginComponent},
     {path: "auth/register", component: RegisterComponent},
     {path: "dashboard/home", component: DashboardComponent},
-    {path: "dashboard/agents", component: AgentsComponent},
+    {path: "dashboard/agents", component: GestionAgentsComponent},
+
 ];

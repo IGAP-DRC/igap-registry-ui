a070e972e43570329f143ef4368427408fdc61ae
Joe Monkila <jnkiwa25@gmail.com>
2024-12-09 10:20:46 +0100

    agents service and add view data from database

--- src/app/presentation/dashboard/pages/gestion-agents/gestion-agents.component.ts
+++ src/app/presentation/dashboard/pages/gestion-agents/gestion-agents.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../../../shared/header/header.component";
 import { SideNavComponent } from "../../../shared/side-nav/side-nav.component";
 import { FooterComponent } from "../../../shared/footer/footer.component";
 import { AgentsComponent } from "../../../shared/agents-form/agents.component";
+import { AgentService } from '../../../../core/agents/agent.service';
 
 @Component({
   selector: 'app-gestion-agents',
@@ -11,6 +12,24 @@ import { AgentsComponent } from "../../../shared/agents-form/agents.component";
   templateUrl: './gestion-agents.component.html',
   styleUrl: './gestion-agents.component.css'
 })
-export class GestionAgentsComponent {
+export class GestionAgentsComponent implements OnInit{
+  
+  data:any;
 
+  constructor(private agent:AgentService){}
+  
+  ngOnInit(): void {
+    this.onGetData();
+  }
+
+  onGetData(){
+    this.agent.getAllAgents().subscribe({
+      next: (response)=>{
+        this.data = response;
+      },
+      error: (error)=>{
+        console.log(error);
+      }
+    });
+  }
 }
